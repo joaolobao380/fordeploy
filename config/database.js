a80f8f97027a32862fdbd13e43e3e90a427dd186
joaolobao380 <victorlobao380@gmail.com>
2021-08-25 23:50:39 -0300

    fix database

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'h1sys_strapi'),
+        username: env('DATABASE_USERNAME', 'h1sys'),
+        password: env('DATABASE_PASSWORD', 'tr-server'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
