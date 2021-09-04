9d4c49ed441520da75b07816b25d5d13d52426eb
joaolobao380 <joao.lobao@ilia.digital>
2021-09-04 01:09:57 -0300

    fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,7 +10,9 @@ module.exports = ({ env }) => ({
           database: env('DATABASE_NAME'),
           username: env('DATABASE_USERNAME'),
           password: env('DATABASE_PASSWORD'),
-          ssl: env.bool('DATABASE_SSL', false),
+          ssl: {
+              rejectUnauthorized: false,
+          },
         },
         options: {}
       },
