1e5d14d215f25624fb6f4bb699cbd7f3747d0ac5
joaolobao380 <joao.lobao@ilia.digital>
2021-09-03 22:45:00 -0300

    variable ambient

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'h1sys_strapi'),
-        username: env('DATABASE_USERNAME', 'h1sys'),
-        password: env('DATABASE_PASSWORD', 'tr-server'),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
